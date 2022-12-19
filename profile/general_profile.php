2acbf28afa7c6ff341337dfaaf736abf33742710
shouhei <shohei.nagamura@gmail.com>
2022-12-20 06:34:04 +0900

    一般ユーザー情報表示機能実装

---
+++ profile/general_profile.php
@@ -0,0 +1,139 @@
+<?php
+
+session_start();
+
+include('../functions/check_session_id.php');
+include('../functions/connect_to_db.php');
+
+if ($_SESSION['user_type'] == 0) {
+    general_check_session_id();
+} else {
+    header("Location:../login/loginTop.php");
+    exit();
+}
+
+$id = $_SESSION['id'];
+
+$email = $_SESSION['email'];
+
+
+// DB接続
+$pdo = connect_to_db();
+// $sql = 'SELECT * FROM pailot_info WHERE user_id=:id';
+$sql = 'SELECT * FROM users INNER JOIN general_info ON users.id = general_info.user_id  where users.id=:id ;';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+try {
+    $status = $stmt->execute();
+} catch (PDOException $e) {
+    echo json_encode(["sql error" => "{$e->getMessage()}"]);
+    exit();
+}
+
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// echo '<pre>';
+// var_dump($result);
+// echo '</pre>';
+// exit();
+
+$json_result = json_encode(($result));
+
+
+// ヘッダー用
+if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 1) {
+    $headerOutput = "
+        <header>
+            <p> LOGO</p>
+            <h1>AIR BASE</h1>
+            <div class='header-nav'>
+                <p>{$email}</p>
+                <a href='../logout/logout.php'>
+                    <p>ログアウト</p>
+                </a>
+            </div>
+        </header>
+
+        <ul>
+            <a href=''>
+                <li>案件検索</li>
+            </a>
+            <a href=''>
+                <li>パイロット検索</li>
+            </a>
+            <a href=''>
+                <li>気に入った案件</li>
+            </a>
+            <a href=''>
+                <li>受注管理</li>
+            </a>
+            <a href='profile.php'>
+                <li>プロフィール</li>
+            </a>
+        </ul>";
+} else if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 0) {
+    $headerOutput = "
+        <header>
+            <p> LOGO</p>
+            <h1>AIR BASE</h1>
+            <div class='header-nav'>
+                <p>{$email}</p>
+                <a href='../logout/logout.php'>
+                    <p>ログアウト</p>
+                </a>
+            </div>
+        </header>
+
+        <ul>
+            <a href=''>
+                <li>案件検索</li>
+            </a>
+            <a href=''>
+                <li>パイロット検索</li>
+            </a>
+            <a href=''>
+                <li>気になるパイロット</li>
+            </a>
+            <a href=''>
+                <li>案件管理</li>
+            </a>
+            <a href='profile.php'>
+                <li>プロフィール</li>
+            </a>
+        </ul>
+        ";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/top.css">
+    <title>AIR BASE</title>
+</head>
+
+<body>
+
+    <?= $headerOutput ?>
+
+
+    <p>一般ユーザープロフィール</p>
+
+    <p><?= $result['name'] ?></p>
+    <p><?= $result['nickname'] ?></p>
+    <p><?= $result['age'] ?></p>
+    <p><?= $result['gender'] ?></p>
+
+    <a href="../Update/userEdit.php">
+        <p>プロフィール更新</p>
+    </a>
+
+</body>
+
+</html>
