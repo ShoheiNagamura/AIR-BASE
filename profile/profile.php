20c353cc624140f65735836c27be076b56f56435
shouhei <shohei.nagamura@gmail.com>
2022-12-19 10:55:17 +0900

    ログアウト機能実装

--- profile/profile.php
+++ profile/profile.php
@@ -14,17 +14,7 @@ if ($_SESSION['user_type'] == 1) {
 
 $id = $_SESSION['id'];
 
-// echo '<pre>';
-// var_dump($result);
-// echo '</pre>';
-// exit();
 
-// //値があったら
-// if (isset($result['name'])) {
-//     header("Location:./profile.php");
-// } else {
-//     header("Location:./profile_input.php");
-// }
 
 
 // DB接続
@@ -50,6 +40,73 @@ $result = $stmt->fetch(PDO::FETCH_ASSOC);
 $json_result = json_encode(($result));
 
 
+// ヘッダー用
+if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 1) {
+    $headerOutput = '
+        <header>
+            <p> LOGO</p>
+            <h1>AIR BASE</h1>
+            <div class="header-nav">
+                <!-- <a href="#"><img src="" alt=""></a> -->
+                <p>ユーザー</p>
+                <a href="../logout/logout.php">
+                    <p>ログアウト</p>
+                </a>
+            </div>
+        </header>
+
+        <ul>
+            <a href="">
+                <li>案件検索</li>
+            </a>
+            <a href="">
+                <li>パイロット検索</li>
+            </a>
+            <a href="">
+                <li>気に入った案件</li>
+            </a>
+            <a href="">
+                <li>受注管理</li>
+            </a>
+            <a href="profile.php">
+                <li>プロフィール</li>
+            </a>
+        </ul>';
+} else if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 0) {
+    $headerOutput = '
+        <header>
+            <p> LOGO</p>
+            <h1>AIR BASE</h1>
+            <div class="header-nav">
+                <!-- <a href="#"><img src="" alt=""></a> -->
+                <p>ユーザー</p>
+                <a href="../logout/logout.php">
+                    <p>ログアウト</p>
+                </a>
+            </div>
+        </header>
+
+        <ul>
+            <a href="">
+                <li>案件検索</li>
+            </a>
+            <a href="">
+                <li>パイロット検索</li>
+            </a>
+            <a href="">
+                <li>気になるパイロット</li>
+            </a>
+            <a href="">
+                <li>案件管理</li>
+            </a>
+            <a href="profile.php">
+                <li>プロフィール</li>
+            </a>
+        </ul>
+        ';
+}
+
+
 ?>
 
 
@@ -66,32 +123,9 @@ $json_result = json_encode(($result));
 </head>
 
 <body>
-    <header>
-        <p> LOGO</p>
-        <h1>AIR BASE</h1>
-        <div class="header-nav">
-            <!-- <a href="#"><img src="" alt=""></a> -->
-            <p>ユーザー名</p>
-        </div>
-    </header>
 
-    <ul>
-        <a href="">
-            <li>案件検索</li>
-        </a>
-        <a href="">
-            <li>パイロット検索</li>
-        </a>
-        <a href="">
-            <li>気に入った案件</li>
-        </a>
-        <a href="">
-            <li>受注管理</li>
-        </a>
-        <a href="./profile.php">
-            <li>プロフィール</li>
-        </a>
-    </ul>
+    <?= $headerOutput ?>
+
 
     <p>パイロットプロフィール</p>
 
